30f3dc54b25e9a2fa88d8535e85501112343a2f5
Jordan Bryan <jbryan382@gmail.com>
2019-03-18 15:13:27 -0400

    Set up basic table for safari adventure

---
+++ Animal.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SafariAdventure
+{
+  class Animal
+  {
+    public int Id { get; set; }
+    public string Species { get; set; }
+    public int CountOfTimesSeen { get; set; }
+    public string LocationOfLastSeen { get; set; }
+  }
+}
